7b0737c829c6cdaeb0b4a7f3ce8972eea218d0ad
Jeffrey Mann <jwmann22@gmail.com>
2019-11-23 19:41:58 -0500

    wrote server file

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const exphbrs = require('express-handlebars')
+require('dotenv').config();
+
+const PORT = process.env.PORT || 8000;
+
+let app = express();
+
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.engine("handlebars", exphbrs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+let routes = require('./controllers/burger_controller');
+
+app.use(routes);
+
+app.listen(PORT, (err) => {
+    if (err) throw err;
+
+    console.log("app listening on http://localhost:" + PORT);
+})
