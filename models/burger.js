9bd48038ef976e0a712572c5afa1241e7a35dede
Jeffrey Mann <jwmann22@gmail.com>
2019-12-03 23:26:26 -0500

    customer association feature added doesnt look pretty

--- models/burger.js
+++ models/burger.js
@@ -1,8 +1,20 @@
-module.exports = function(sequelize, DataTypes) {
-    let Burger = sequelize.define("burgers", {
-        burger_name: DataTypes.STRING,
+module.exports = function (sequelize, DataTypes) {
+    var Burger = sequelize.define("Burger", {
+        burger_name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [1]
+            }
+        },
         devoured: DataTypes.BOOLEAN
     });
+
+    Burger.associate = function (models) {
+        Burger.hasMany(models.Customer, {
+            onDelete: "cascade"
+        })
+    }
 
     return Burger;
 }
